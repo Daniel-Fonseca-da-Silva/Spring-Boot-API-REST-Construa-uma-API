3bf95d55113775b05e64bc004cc952f53d8c41cb
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-03-06 17:32:46 -0300

    Validações com Bean Validation

--- src/main/java/com/projeto/forum/controller/form/TopicoForm.java
+++ src/main/java/com/projeto/forum/controller/form/TopicoForm.java
@@ -4,10 +4,19 @@ import com.projeto.forum.modelo.Curso;
 import com.projeto.forum.modelo.Topico;
 import com.projeto.forum.repository.CursoRepository;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Length;
+
 public class TopicoForm {
 
+    @NotNull @NotEmpty @Size(min = 5)
     private String titulo;
+    @NotNull @NotEmpty @Size(min = 10)
     private String mensagem;
+    @NotNull @NotEmpty
     private String nomecurso;
 
     public String getTitulo() {
